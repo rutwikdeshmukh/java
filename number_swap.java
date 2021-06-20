7ee4d1cd0aefd665e121a01552faeecfefdccf16
Rutwik Deshmukh <68472165+rutwikdeshmukh@users.noreply.github.com>
2021-06-20 15:25:29 +0530

    Add files via upload

---
+++ number_swap.java
@@ -0,0 +1,16 @@
+package day3;
+
+public class number_swap {
+	
+	public static void main(String[] args) {
+		int a=10 ; int b=29; int temp;
+        System.out.println("A= " + a + " B= " + b);
+
+		temp = a;
+		a = b;
+		b = temp ;
+		System.out.println("A= " + a + " B= " + b);
+		
+	}
+
+}
